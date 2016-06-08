0ff04736c9fc62366117d303b2150dd3caa91066
jtkaufman737 <jtkaufman737@gmail.com>
2016-06-08 14:06:10 -0400

    Post lunch updates

--- main.js
+++ main.js
@@ -1,10 +1,38 @@
+
 window.onload = function() {	
 
 	var startButton = document.querySelector('button');
 	startButton.onclick = function() {
 		start();
-		console.log(gameOn);
+		console.log(start);
 	}
 } 
 
-/*Remember for later: use combination of pop and push as possible way to shuffle the cards? Wouldn't be dry.
+var cards = ['A', 'A','B', 'B', 'C', 'C', 'D', 'D', 'E', 'E'];
+	console.log(cards);
+
+function shuffle(o) { 
+	for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+		return o;
+}
+
+function start(){
+	console.log('start button clicked!');
+	shuffle(cards);
+	console.log(cards);
+	setUpCards();
+	console.log(setUpCards);
+}
+
+function setUpCards() {
+	console.log('make'); 
+
+    for (var index=0; index<cards.length; index++) {
+		var cards = document.createElement('carDiv');
+		cards.setAttribute('class','column');
+		cards.setAttribute('data-value',tiles[index])
+		cards.onclick = function() {
+			makePlay(this);
+			}
+		}
+	}
