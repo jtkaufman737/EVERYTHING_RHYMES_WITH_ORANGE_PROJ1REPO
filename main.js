4a52f79edfa861ed74c505a2aff7c12a4fe41abd
jtkaufman737 <jtkaufman737@gmail.com>
2016-06-09 14:40:59 -0400

    Adding to branch pages

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 window.onload = function() {	
 
 	var startButton = document.querySelector('button');
@@ -26,64 +27,68 @@ function start(){
 function setUpCards() {
 	console.log('make'); 
 
-for (var index=0; index<cards.length; index++) {
-		var cards = document.createElement('carDiv');
-		cards.setAttribute('class','column');
+for (var index=0; index<10; index++) {
+		var cardCreateImage = document.getElementByClass('carDiv');
+		cards.setAttribute('class',column);
 		cards.setAttribute('data-value',cards[index]);
 		cards.onclick = function() {
 			makePlay(this);
 			};
 		}
 	}
-function makePlay(tile){
-	function showLetter () {
-	getElementById(.tiles)
 
-	//*something to display inner html
-    carDiv.onclick('attribute','clicked')
+// function makePlay(cards){
+// 	function showLetter () {
+// 	getElementById(cards);
+
+// 	//*something to display inner html
+//     carDiv.onclick('attribute',clicked);
     
 	
-	console.log(cards);
-	var function = twoClicked () {
-		for var twoClicked (i<3; i++) //starts a counter
-			//*how do I reference the number of elements with an attribute
-			//*to tell if the attribute "clicked" exists twice?
-	}
+	
+	// var twoClicked= twoClicked (); {
+	// 	for (i<3; i++;) 
+	// }
 
-function checkForMatch() {
+// var checkForMatch=function() {
 
-	// write all your code for this function inside setTimeout below:
+// 	// write all your code for this function inside setTimeout below:
 
-	setTimeout function() //*This syntax looks weird--did I somehow accientally do that?
-		//* trying to get the data value out of 2 clicked elements. For 
-		//* while they are equal, else if not...
-		while {
-			getElementByID('clicked');
-			(clicked(data-value) == clicked(data-value)) == true;
-			clicked.setAttribute('class', 'found');
-			remove.attribute('class','clicked')
-		}
-		else if {
-			getElementByID('clicked');
-			(clicked(data-value) !== clicked(data-value)) == true;
-			remove.attribute('class', 'clicked');
-			clicked.innerHTML=('')
-			//* Note to self need to look up syntax on line 81
-			//*Trying to say in this section if the data values are 
-			//*Same add a class of matched, but I'm not sure if I should
-			//* leave the set class on line 82 to set it to nothing, or 
-			//* just have a unspecified else so it won't do anything if
-			//*there is no match
-		}
+//   // setTimeout function() {
+//   // }
+  
+//   //*This syntax looks weird--did I somehow accientally do that?
+// 		//* trying to get the data value out of 2 clicked elements. For 
+// 		//* while they are equal, else if not...
 		
-	}, 1500);
-}
+// //   if {
+// // 			getElementByID('clicked');
+// // 			(clicked(data-value) == clicked(data-value)) === true;
+// // 			clicked.setAttribute('class', 'found');
+// // 			remove.attribute('class','clicked')
+// // 		}
+// // 	else if {
+// // 			getElementByID('clicked');
+// // 			(clicked(data-value) !== clicked(data-value)) == true;
+// // 			remove.attribute('class', 'clicked');
+// // 			clicked.innerHTML=('')
+// // 			//* Note to self need to look up syntax on line 81
+// // 			//*Trying to say in this section if the data values are 
+// // 			//*Same add a class of matched, but I'm not sure if I should
+// // 			//* leave the set class on line 82 to set it to nothing, or 
+// // 			//* just have a unspecified else so it won't do anything if
+// // 			//*there is no match
+// // 		}
+		
+// // 	}, 1500);
+// // }
 
-function checkForWin() {
-  	getElementById('cards')
-  	for //* tiles class =found === true, tiles class === "won" {
+// // function checkForWin() {
+// //   	getElementById('tiles')
+// //   	for //* tiles class =found === true, tiles class === "won" {
   	
-  	}
-  	else {
+// //   	}
+// //   	else {
 
-  	}
+// //   	}
+// //       
